606a71ba0187a27d6f06c937196902c6de778977
Lucky Jha <jhalucky61@gmail.com>
2026-07-06 09:50:20 +0530

    generated orders.csv

---
+++ src/data_generation/generate_order_items.py
@@ -0,0 +1,13 @@
+import csv
+import random
+from faker import Faker
+
+fake = Faker()
+
+
+def generate_order_items(num_order_items, output_file):
+
+    with open(output_file, "w", newline=)
+
+
+    
---
+++ src/data_generation/generate_orders.py
@@ -0,0 +1,51 @@
+import random
+import csv
+from faker import Faker
+from constants import random_order_status
+
+
+fake = Faker()
+
+def generate_orders(num_orders, output_file):
+
+    with open(output_file, "w", newline="", encoding="utf-8") as file:
+
+        writer = csv.writer(file)
+        writer.writerow([
+            "order_id",
+            "customer_id",
+            "order_date",
+            "order_status",
+            "total_amount"
+        ])
+
+
+        for order_id in range(1, num_orders+1):
+
+            writer.writerow([
+                order_id,
+                random.randint(1,1001),
+                fake.date_between(start_date="-3y",end_date="today"),
+                random_order_status(),
+                random.randint(100,100000)
+
+            ])
+
+
+
+    print(f"{num_orders} generated successfully.")
+
+
+
+if __name__ == "__main__":
+    generate_orders(num_orders=1000, output_file="data/raw/orders.csv")
+
+
+
+    
+
+
+
+
+
+
